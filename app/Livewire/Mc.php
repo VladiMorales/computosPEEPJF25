1f2d6b6452f22b3c9144482d5ec6f2a39a3f6f80
Vladimir <moralesvlady2@gmail.com>
2025-11-04 11:51:25 -0600

    Ajuste en MC LiveWire

--- app/Livewire/Mc.php
+++ app/Livewire/Mc.php
@@ -4,7 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Casilla;
 use App\Models\Error;
-use App\Models\Mc as ModelsMc;
+use App\Models\MC as ModelsMc;
 use App\Models\RNU;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
